9b5b1c5699b28025f39efb102efc7cbc91b3e17b
ELghazX <2309106041@student.unmul.ac.id>
2025-05-01 15:26:25 +0800

    feat: setter getter

--- src/main/java/cosplay/spring/core/entity/Kostum.java
+++ src/main/java/cosplay/spring/core/entity/Kostum.java
@@ -5,13 +5,10 @@ import lombok.*;
 
 @Entity
 @Table(name = "kostum")
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Kostum {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,4 +24,61 @@ public class Kostum {
     private Double hargaSewaHarian;
 
     private Double hargaSewaMingguan;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getUkuran() {
+        return ukuran;
+    }
+
+    public void setUkuran(String ukuran) {
+        this.ukuran = ukuran;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public Double getHargaSewaHarian() {
+        return hargaSewaHarian;
+    }
+
+    public void setHargaSewaHarian(Double hargaSewaHarian) {
+        this.hargaSewaHarian = hargaSewaHarian;
+    }
+
+    public Double getHargaSewaMingguan() {
+        return hargaSewaMingguan;
+    }
+
+    public void setHargaSewaMingguan(Double hargaSewaMingguan) {
+        this.hargaSewaMingguan = hargaSewaMingguan;
+    }
+
 }
