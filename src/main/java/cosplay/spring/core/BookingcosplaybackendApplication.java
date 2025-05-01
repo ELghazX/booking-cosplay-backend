2ad80c32fe9c96f75ccd8ec37a3f9946126e3461
ELghazX <2309106041@student.unmul.ac.id>
2025-05-01 09:44:11 +0800

    chore: init spring

---
+++ src/main/java/cosplay/spring/core/BookingcosplaybackendApplication.java
@@ -0,0 +1,13 @@
+package cosplay.spring.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingcosplaybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingcosplaybackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cosplay/spring/core/BookingcosplaybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cosplay.spring.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingcosplaybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
