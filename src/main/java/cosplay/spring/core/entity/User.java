276b93c81a3da8bce681926c423f8578db99b8ee
GalaxionZero <zetayu24@gmail.com>
2025-05-15 20:44:59 +0800

    I don't know what this does

--- src/main/java/cosplay/spring/core/entity/User.java
+++ src/main/java/cosplay/spring/core/entity/User.java
@@ -21,22 +21,10 @@ public class User {
     private String password;
     private String email;
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
--- src/main/java/cosplay/spring/core/repository/UserRepository.java
+++ src/main/java/cosplay/spring/core/repository/UserRepository.java
@@ -3,8 +3,8 @@ package cosplay.spring.core.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import cosplay.spring.core.entity.Kostum;
+import cosplay.spring.core.entity.User;
 
 @Repository
-public interface KostumRepository extends JpaRepository<Kostum, Long> {
+public interface UserRepository extends JpaRepository<User, Long>{
 }
