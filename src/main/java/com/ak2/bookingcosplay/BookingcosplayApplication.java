0edae58a8e31e281060de420c1ff5c78cac2a122
ELghazX <2309106041@student.unmul.ac.id>
2025-05-20 11:16:48 +0800

    reinit project

---
+++ src/main/java/com/ak2/bookingcosplay/BookingcosplayApplication.java
@@ -0,0 +1,13 @@
+package com.ak2.bookingcosplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingcosplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingcosplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ak2/bookingcosplay/BookingcosplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ak2.bookingcosplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingcosplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
