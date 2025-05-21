7e9b037e206c1f94a2360cfa854bbccc58525f23
ELghazX <2309106041@student.unmul.ac.id>
2025-05-21 20:58:41 +0800

    Delete: buat ulang

--- src/main/java/com/ak2/bookingcosplay/repository/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ak2.bookingcosplay.repository;
-
-import com.ak2.bookingcosplay.entity.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
