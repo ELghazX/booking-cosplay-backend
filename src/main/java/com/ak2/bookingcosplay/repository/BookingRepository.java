0dcde4e4e6847eb85578d9e83edce3caf8fcde5e
ELghazX <2309106041@student.unmul.ac.id>
2025-05-21 11:20:40 +0800

    repository: jpa booking

---
+++ src/main/java/com/ak2/bookingcosplay/repository/BookingRepository.java
@@ -0,0 +1,7 @@
+package com.ak2.bookingcosplay.repository;
+
+import com.ak2.bookingcosplay.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+}
