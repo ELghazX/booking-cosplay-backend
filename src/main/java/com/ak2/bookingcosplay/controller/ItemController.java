d7eac5bc2bd01229cd9fdefddacc75f284a56528
Zhorif Fachdiat <zhorif.f.32@gmail.com>
2025-05-27 18:55:20 +0800

    ubah item control

--- src/main/java/com/ak2/bookingcosplay/controller/ItemController.java
+++ src/main/java/com/ak2/bookingcosplay/controller/ItemController.java
@@ -1,5 +1,8 @@
 package com.ak2.bookingcosplay.controller;
 
+import java.io.IOException;
+import java.io.File;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ak2.bookingcosplay.dto.RequestCreateAccessory;
 import com.ak2.bookingcosplay.dto.RequestCreateCostume;
@@ -46,64 +51,120 @@ public class ItemController {
     return ResponseEntity.ok(response);
   }
 
-  @PostMapping("/create-costume")
-  public ResponseEntity<ResponseDefault> createCostume(@RequestBody RequestCreateCostume request) {
+  @PostMapping(value = "/create-costume", consumes = {"multipart/form-data"})
+  public ResponseEntity<ResponseDefault> createCostume(
+    @RequestParam("name") String name,
+    @RequestParam("pricePerDay") int pricePerDay,
+    @RequestParam("size") String size,
+    @RequestParam("gender") String gender,
+    @RequestParam("characterName") String characterName,
+    @RequestParam("image") MultipartFile imageFile
+  ) throws IOException {
+    String uploadDir = "uploads/";
+    String fileName = System.currentTimeMillis() + "_" + imageFile.getOriginalFilename();
+    File dest = new File(uploadDir + fileName);
+    dest.getParentFile().mkdirs();
+    imageFile.transferTo(dest);
+
     Costume costume = new Costume();
-    costume.setName(request.getName());
-    costume.setImageUrl(request.getImageUrl());
-    costume.setPricePerDay(request.getPricePerDay());
-    costume.setSize(request.getSize());
-    costume.setGender(request.getGender());
-    costume.setCharacterName(request.getCharacterName());
+    costume.setName(name);
+    costume.setImageUrl("/uploads/" + fileName); // Simpan path gambar
+    costume.setPricePerDay(pricePerDay);
+    costume.setSize(size);
+    costume.setGender(gender);
+    costume.setCharacterName(characterName);
+
     ResponseDefault response = itemService.createItem(costume);
-    if (response.isStatus() == false) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    if (!response.isStatus()) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
     return ResponseEntity.ok(response);
   }
 
-  @PostMapping("/create-accessory")
-  public ResponseEntity<ResponseDefault> createAccessory(@RequestBody RequestCreateAccessory request) {
+  @PostMapping(value = "/create-accessory", consumes = {"multipart/form-data"})
+  public ResponseEntity<ResponseDefault> createAccessory(
+    @RequestParam("name") String name,
+    @RequestParam("pricePerDay") int pricePerDay,
+    @RequestParam("type") String type,
+    @RequestParam("image") MultipartFile imageFile
+  ) throws IOException {
+    String uploadDir = "uploads/";
+    String fileName = System.currentTimeMillis() + "_" + imageFile.getOriginalFilename();
+    File dest = new File(uploadDir + fileName);
+    dest.getParentFile().mkdirs();
+    imageFile.transferTo(dest);
+
     Accessory accessory = new Accessory();
-    accessory.setName(request.getName());
-    accessory.setImageUrl(request.getImageUrl());
-    accessory.setPricePerDay(request.getPricePerDay());
-    accessory.setType(request.getType());
+    accessory.setName(name);
+    accessory.setImageUrl("/uploads/" + fileName); // Simpan path gambar
+    accessory.setPricePerDay(pricePerDay);
+    accessory.setType(type);
+
     ResponseDefault response = itemService.createItem(accessory);
-    if (response.isStatus() == false) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    if (!response.isStatus()) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
     return ResponseEntity.ok(response);
   }
 
-  @PutMapping("update-accessory/{id}")
-  public ResponseEntity<ResponseDefault> updateAccessory(@PathVariable Long id,
-      @RequestBody RequestCreateAccessory request) {
+  @PutMapping(value = "update-accessory/{id}", consumes = {"multipart/form-data"})
+  public ResponseEntity<ResponseDefault> updateAccessory(
+    @PathVariable Long id,
+    @RequestParam("name") String name,
+    @RequestParam("pricePerDay") int pricePerDay,
+    @RequestParam("type") String type,
+    @RequestParam(value = "image", required = false) MultipartFile imageFile
+  ) throws IOException {
     Accessory accessory = new Accessory();
-    accessory.setName(request.getName());
-    accessory.setImageUrl(request.getImageUrl());
-    accessory.setPricePerDay(request.getPricePerDay());
-    accessory.setType(request.getType());
+    accessory.setName(name);
+    accessory.setPricePerDay(pricePerDay);
+    accessory.setType(type);
+
+    if (imageFile != null && !imageFile.isEmpty()) {
+        String uploadDir = "uploads/";
+        String fileName = System.currentTimeMillis() + "_" + imageFile.getOriginalFilename();
+        File dest = new File(uploadDir + fileName);
+        dest.getParentFile().mkdirs();
+        imageFile.transferTo(dest);
+        accessory.setImageUrl("/uploads/" + fileName);
+    }
+
     ResponseDefault response = itemService.updateItem(id, accessory);
-    if (response.isStatus() == false) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    if (!response.isStatus()) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
     return ResponseEntity.ok(response);
   }
 
-  @PutMapping("update-costume/{id}")
-  public ResponseEntity<ResponseDefault> updateCostume(@PathVariable Long id,
-      @RequestBody RequestCreateCostume request) {
+  @PutMapping(value = "update-costume/{id}", consumes = {"multipart/form-data"})
+  public ResponseEntity<ResponseDefault> updateCostume(
+    @PathVariable Long id,
+    @RequestParam("name") String name,
+    @RequestParam("pricePerDay") int pricePerDay,
+    @RequestParam("size") String size,
+    @RequestParam("gender") String gender,
+    @RequestParam("characterName") String characterName,
+    @RequestParam(value = "image", required = false) MultipartFile imageFile
+  ) throws IOException {
     Costume costume = new Costume();
-    costume.setName(request.getName());
-    costume.setImageUrl(request.getImageUrl());
-    costume.setPricePerDay(request.getPricePerDay());
-    costume.setSize(request.getSize());
-    costume.setGender(request.getGender());
-    costume.setCharacterName(request.getCharacterName());
+    costume.setName(name);
+    costume.setPricePerDay(pricePerDay);
+    costume.setSize(size);
+    costume.setGender(gender);
+    costume.setCharacterName(characterName);
+
+    if (imageFile != null && !imageFile.isEmpty()) {
+        String uploadDir = "uploads/";
+        String fileName = System.currentTimeMillis() + "_" + imageFile.getOriginalFilename();
+        File dest = new File(uploadDir + fileName);
+        dest.getParentFile().mkdirs();
+        imageFile.transferTo(dest);
+        costume.setImageUrl("/uploads/" + fileName);
+    }
+
     ResponseDefault response = itemService.updateItem(id, costume);
-    if (response.isStatus() == false) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    if (!response.isStatus()) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
     return ResponseEntity.ok(response);
   }
